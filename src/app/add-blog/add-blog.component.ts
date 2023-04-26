3f9e77234e98e044354140d2ecac804fabb53093
Manoj M <manumadhu1425@gmail.com>
2023-04-26 10:46:51 +0530

    Blog Adding feature

---
+++ src/app/add-blog/add-blog.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { blog } from '../view-blogs/blog/blog.interface';
+import { NgForm } from '@angular/forms';
+import { BlogAppService } from '../services/blog-app.service';
+
+@Component({
+  selector: 'app-add-blog',
+  templateUrl: './add-blog.component.html',
+  styleUrls: ['./add-blog.component.css'],
+})
+export class AddBlogComponent {
+  contextLen: string = '';
+  blogAdded: boolean = false;
+  title: string;
+
+  constructor(private blogService: BlogAppService) {}
+
+  onBlogSubmit(blogData: blog, form: NgForm) {
+    this.blogService.createPost(blogData).subscribe(() => {
+      this.title = blogData.title;
+      this.blogAdded = true;
+      form.reset();
+      this.contextLen = '';
+    });
+  }
+}

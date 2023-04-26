aef2cf2c430c430ec25cbf595a8bd36d0081f74e
Manoj M <manumadhu1425@gmail.com>
2023-04-26 10:47:44 +0530

    Component to view blog

---
+++ src/app/view-blogs/view-blogs.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { BlogAppService } from '../services/blog-app.service';
+import { blog } from './blog/blog.interface';
+
+@Component({
+  selector: 'app-view-blogs',
+  templateUrl: './view-blogs.component.html',
+  styleUrls: ['./view-blogs.component.css'],
+})
+export class ViewBlogsComponent implements OnInit {
+  constructor(private blogService: BlogAppService) {}
+  ngOnInit(): void {
+    this.fetchposts();
+  }
+
+  blogs: blog[] = [];
+
+  fetchposts() {
+    this.blogService.fetchposts().subscribe((response) => {
+      this.blogs = response;
+    });
+  }
+}

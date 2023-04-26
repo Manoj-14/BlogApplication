1849f0e4bc2015498188ef684f2b998ed9295cbe
Manoj M <manumadhu1425@gmail.com>
2023-04-26 10:45:51 +0530

    Adding Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddBlogComponent } from './add-blog/add-blog.component';
+import { FormsModule } from '@angular/forms';
+import { ViewBlogsComponent } from './view-blogs/view-blogs.component';
+import { BlogComponent } from './view-blogs/blog/blog.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ContentLengthPipe } from './pipes/content-length.pipe';
+
+const appRouts: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'AddBlog', component: AddBlogComponent },
+  { path: 'ViewBlogs', component: ViewBlogsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddBlogComponent,
+    ViewBlogsComponent,
+    BlogComponent,
+    ContentLengthPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouts),
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

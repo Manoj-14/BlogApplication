1a0e5b0311564d7d4f324adea69dc605fcb1ccfb
Manoj M <manumadhu1425@gmail.com>
2023-04-26 10:47:25 +0530

    Each blog component

---
+++ src/app/view-blogs/blog/blog.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { blog } from './blog.interface';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css'],
+})
+export class BlogComponent {
+  @Input()
+  blogInp: blog;
+  btnStatus: string = 'Read More';
+
+  contentLen: number = 200;
+
+  @Output()
+  postDeleted = new EventEmitter<{ deleted: boolean }>();
+
+  onExpandMore() {
+    if (this.contentLen == 200) {
+      this.contentLen = this.blogInp.content.length;
+      this.btnStatus = 'Read Less';
+    } else {
+      this.contentLen = 200;
+      this.btnStatus = 'Read More';
+    }
+  }
+}
